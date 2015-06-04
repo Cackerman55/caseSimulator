4ba6b6e7d0b5a6f3206fbd293f3e42c867b89a28
Colin Ackerman <cackerman@sjci.com>
2015-06-04 11:31:28 -0400

    case windows setup
    FOR ALL CASE WINDOWS
    -added backgrounds
    -added text fields for data
    -added individual headers
    -fixed minimum window size

--- src/master/cases/basic_cases/chroma2.java
+++ src/master/cases/basic_cases/chroma2.java
@@ -27,18 +27,128 @@ public class chroma2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jTextField9 = new javax.swing.JTextField();
+        jTextField10 = new javax.swing.JTextField();
+        jTextField11 = new javax.swing.JTextField();
+        jTextField12 = new javax.swing.JTextField();
+        jTextField7 = new javax.swing.JTextField();
+        jTextField8 = new javax.swing.JTextField();
+        jTextField6 = new javax.swing.JTextField();
+        jTextField5 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(1080, 735));
+        getContentPane().setLayout(null);
+
+        jTextField9.setEditable(false);
+        jTextField9.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField9.setText("jTextField1");
+        jTextField9.setBorder(null);
+        jTextField9.setOpaque(false);
+        getContentPane().add(jTextField9);
+        jTextField9.setBounds(1040, 260, 30, 60);
+
+        jTextField10.setEditable(false);
+        jTextField10.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField10.setText("jTextField1");
+        jTextField10.setBorder(null);
+        jTextField10.setOpaque(false);
+        getContentPane().add(jTextField10);
+        jTextField10.setBounds(1000, 260, 30, 60);
+
+        jTextField11.setEditable(false);
+        jTextField11.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField11.setText("jTextField1");
+        jTextField11.setBorder(null);
+        jTextField11.setOpaque(false);
+        getContentPane().add(jTextField11);
+        jTextField11.setBounds(1040, 250, 30, 30);
+
+        jTextField12.setEditable(false);
+        jTextField12.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField12.setText("jTextField1");
+        jTextField12.setBorder(null);
+        jTextField12.setOpaque(false);
+        getContentPane().add(jTextField12);
+        jTextField12.setBounds(1000, 250, 30, 30);
+
+        jTextField7.setEditable(false);
+        jTextField7.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField7.setText("jTextField1");
+        jTextField7.setBorder(null);
+        jTextField7.setOpaque(false);
+        getContentPane().add(jTextField7);
+        jTextField7.setBounds(1040, 220, 30, 30);
+
+        jTextField8.setEditable(false);
+        jTextField8.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField8.setText("jTextField1");
+        jTextField8.setBorder(null);
+        jTextField8.setOpaque(false);
+        getContentPane().add(jTextField8);
+        jTextField8.setBounds(1000, 220, 30, 30);
+
+        jTextField6.setEditable(false);
+        jTextField6.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField6.setText("jTextField1");
+        jTextField6.setBorder(null);
+        jTextField6.setOpaque(false);
+        getContentPane().add(jTextField6);
+        jTextField6.setBounds(1040, 190, 30, 30);
+
+        jTextField5.setEditable(false);
+        jTextField5.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField5.setText("jTextField1");
+        jTextField5.setBorder(null);
+        jTextField5.setOpaque(false);
+        getContentPane().add(jTextField5);
+        jTextField5.setBounds(1000, 190, 30, 30);
+
+        jTextField4.setEditable(false);
+        jTextField4.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField4.setText("jTextField1");
+        jTextField4.setBorder(null);
+        jTextField4.setOpaque(false);
+        getContentPane().add(jTextField4);
+        jTextField4.setBounds(1010, 130, 60, 30);
+
+        jTextField3.setEditable(false);
+        jTextField3.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField3.setText("jTextField1");
+        jTextField3.setBorder(null);
+        jTextField3.setOpaque(false);
+        getContentPane().add(jTextField3);
+        jTextField3.setBounds(990, 100, 80, 30);
+
+        jTextField2.setEditable(false);
+        jTextField2.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField2.setText("jTextField1");
+        jTextField2.setBorder(null);
+        jTextField2.setOpaque(false);
+        getContentPane().add(jTextField2);
+        jTextField2.setBounds(990, 70, 80, 30);
+
+        jTextField1.setEditable(false);
+        jTextField1.setFont(new java.awt.Font("OCR A Std", 0, 16)); // NOI18N
+        jTextField1.setText("jTextField1");
+        jTextField1.setBorder(null);
+        jTextField1.setOpaque(false);
+        getContentPane().add(jTextField1);
+        jTextField1.setBounds(1010, 20, 60, 30);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guis/background/chroma2Head.png"))); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(100, 10, 690, 90);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guis/background/case.png"))); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 0, 1080, 700);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -79,5 +189,19 @@ public class chroma2 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField10;
+    private javax.swing.JTextField jTextField11;
+    private javax.swing.JTextField jTextField12;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField7;
+    private javax.swing.JTextField jTextField8;
+    private javax.swing.JTextField jTextField9;
     // End of variables declaration//GEN-END:variables
 }
